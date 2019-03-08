374b45015f1034876c9c9ed70a70a5ea1bdacc7e
Benjamin Held <me@bheld.eu>
2019-03-08 18:03:32 +0100

    added meta data files

---
+++ data/meta_data/meta_data.rb
@@ -0,0 +1,11 @@
+# @Author: Benjamin Held
+# @Date:   2018-01-27 16:45:47
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2019-03-07 17:57:33
+
+module MetaData
+
+  require_relative 'base_meta_data'
+  require_relative 'wrf_meta_data'
+
+end  
