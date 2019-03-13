47791adec02b6f50ea3cc3e285dc7dc232605da5
Benjamin Held <me@bheld.eu>
2019-03-13 19:39:47 +0100

    added convert files

---
+++ convert/base_converter.rb
@@ -0,0 +1,60 @@
+# @Author: Benjamin Held
+# @Date:   2018-06-23 17:03:05
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2018-07-28 15:04:53
+
+require 'json'
+require_relative '../data/wrf/wrf'
+require_relative '../data/data_repository'
+require_relative '../string/string'
+
+
+class BaseConverter
+
+  def initialize(repository)
+    @repository = repository
+  end
+
+  def convert
+    output = Hash.new()
+
+    output[:meta_data] = generate_meta_hash()
+    output[:weather_data] = generate_data_values()
+    file = File.open("output.json", "w")
+    file.write(JSON.generate(output))
+  end
+
+  private
+
+  attr_reader :repository
+
+  def generate_meta_hash()
+    meta_data = @repository.meta_data
+    meta_hash = Hash.new()
+    meta_hash[:station] = generate_station_hash(meta_data.station)
+    meta_hash[:start_date] = meta_data.start_date
+    meta_hash = meta_hash.merge(add_additions())
+    return meta_hash
+  end
+
+  def generate_station_hash(station)
+    station_hash = Hash.new()
+    station_hash[:name] = station.name
+    station_hash[:descriptor] = station.descriptor
+    station_hash[:elevation] = station.elevation
+    station_hash[:coordinate] = { :x => station.coordinate.x, 
+                                  :y => station.coordinate.y }
+    return station_hash
+  end
+
+  def add_additions()
+    fail NotImplementedError, " Error: the subclass #{self.class} needs " \
+         "to implement the method: add_additions from its base class".red
+  end
+
+  def generate_data_values()
+    fail NotImplementedError, " Error: the subclass #{self.class} needs " \
+         "to implement the method: generate_data_hashes from its base class".red
+  end
+
+end
---
+++ convert/wrf_converter.rb
@@ -0,0 +1,50 @@
+# @Author: Benjamin Held
+# @Date:   2018-07-27 18:54:17
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2018-07-28 15:04:19
+
+require_relative 'base_converter'
+
+class WrfConverter < BaseConverter
+
+  private
+
+  def add_additions()
+      additions = Hash.new()
+      additions[:grid_point] = generate_grid_data(@repository.meta_data.grid_data)
+      additions[:grid_coordinates] = 
+        generate_grid_coordinates(@repository.meta_data.grid_data)
+      return additions
+  end
+
+  def generate_grid_data(grid_point)
+    grid_data = Hash.new()
+    grid_data[:grid_point] = { :x => grid_point.grid_point.x, 
+                               :y => grid_point.grid_point.y }
+    return grid_data
+  end
+
+  def generate_grid_coordinates(grid_point)
+    grid_coordinates = Hash.new()
+    grid_coordinates[:grid_coordinates] = { :x => grid_point.grid_coordinates.x, 
+                                            :y => grid_point.grid_coordinates.y }
+    return grid_coordinates
+  end
+
+  def generate_data_values()
+    data = Array.new()
+    @repository.repository.each { |dataset|
+      data << create_data_hash(dataset)
+    }
+    return data
+  end
+
+  def create_data_hash(dataset)
+    data_entries = Hash.new()
+    dataset.instance_variables.map{|ivar| 
+      data_entries[ivar] = dataset.instance_variable_get(ivar)
+    }
+    return data_entries
+  end
+
+end
