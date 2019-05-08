b2c656fffc68b431e8310559a68ab93a0e2a8513
Benjamin Held <me@bheld.eu>
2019-05-08 15:52:21 +0200

    added time import to required classes

--- data/meta_data/meta_data.rb
+++ data/meta_data/meta_data.rb
@@ -1,10 +1,11 @@
 # @Author: Benjamin Held
 # @Date:   2018-01-27 16:45:47
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-04-12 22:15:13
+# @Last Modified time: 2019-05-08 15:49:59
 
 module MetaData
 
+  require 'time'
   require_relative '../../ruby_utils/data/meta_data'
   require_relative 'base_meta_data'
   require_relative 'wrf_meta_data'
--- data/wrf/wrf.rb
+++ data/wrf/wrf.rb
@@ -1,12 +1,13 @@
 # @Author: Benjamin Held
 # @Date:   2018-01-28 21:19:43
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-03-27 19:46:55
+# @Last Modified time: 2019-05-08 15:50:16
 
 # This module holds the classes handling the representation of the wrf result
 # data
 module Wrf
 
+  require 'time'
   require_relative 'wrf_entry'
   require_relative 'wrf_handler'
 
