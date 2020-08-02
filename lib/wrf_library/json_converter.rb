40b6785d1a8d91c1e375384a5686fc3808ee9c6e
Benjamin Held <me@bheld.eu>
2020-08-02 18:08:09 +0200

    renamed abstract parent class

--- lib/wrf_library/json_converter.rb
+++ lib/wrf_library/json_converter.rb
@@ -1,9 +1,9 @@
 # @Author: Benjamin Held
 # @Date:   2018-06-23 17:03:05
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-03-02 18:47:53
+# @Last Modified time: 2020-08-02 18:05:59
 
 require 'json'
 
-require 'wrf_library/json_converter/station_converter'
+require 'wrf_library/json_converter/base_station_converter'
 require 'wrf_library/json_converter/wrf_converter'
