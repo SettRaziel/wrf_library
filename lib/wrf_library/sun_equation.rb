63f18d6a3eeeb3e616a7124a5d3e362f42d70c06
Benjamin Held <me@bheld.eu>
2021-01-04 19:52:23 +0100

    added helper functions for trigonometric calculations

--- lib/wrf_library/sun_equation.rb
+++ lib/wrf_library/sun_equation.rb
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2020-12-27 14:34:22
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-12-27 22:07:31
+# @Last Modified time: 2021-01-04 19:13:06
 
 require "date"
 
@@ -16,6 +16,20 @@ module WrfLibrary
     # @return [Float] the julian day of the given date since the reference day of jan 1st 2000
     def self.calculate_current_julian_day(date=DateTime.now)
       date.jd().to_f - 2451545.0 + 0.0008
+    end
+
+    # helper method to convert a radiant value into degrees
+    # @param [Float] the angel in radians
+    # @return [Float] the angle in degree
+    def self.convert_radiant_to_degree(angle)
+      angle * 180 / Math::PI
+    end
+
+    # helper method to convert a degree value into radians
+    # @param [Float] the angle in degrees
+    # @return [Float] the angle in radians
+    def self.convert_degree_to_radiant(angle)
+      angle * Math::PI / 180
     end
 
   end
--- spec/sun_equation/sun_equation_spec.rb
+++ spec/sun_equation/sun_equation_spec.rb
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2020-12-27 14:45:56
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-12-27 22:10:01
+# @Last Modified time: 2021-01-04 19:38:40
 
 require "spec_helper"
 require "wrf_library/sun_equation"
@@ -12,7 +12,7 @@ describe WrfLibrary::SunEquation do
   describe "#calculate_current_julian_day" do
     context "given a date" do
       it "calculate the julian day for at in reference to jan 1th 2000" do
-        expect(WrfLibrary::SunEquation.calculate_current_julian_day(DateTime.new(2000,1,1))).to eq(0.0008)
+        expect(WrfLibrary::SunEquation.calculate_current_julian_day(DateTime.new(2000,1,1,12))).to eq(0.0008)
       end
     end
   end
@@ -20,7 +20,55 @@ describe WrfLibrary::SunEquation do
   describe "#calculate_current_julian_day" do
     context "given a date" do
       it "calculate the julian day for at in reference to jan 1th 2000" do
-        expect(WrfLibrary::SunEquation.calculate_current_julian_day(DateTime.new(2014,6,29))).to eq(5293.0008)
+        expect(WrfLibrary::SunEquation.calculate_current_julian_day(DateTime.new(2014,6,29,12))).to eq(5293.0008)
+      end
+    end
+  end
+
+  describe "#convert_radiant_to_degree" do
+    context "given an angle in radians" do
+      it "calculate the corresponding angle in degree" do
+        expect(WrfLibrary::SunEquation.convert_radiant_to_degree(Math::PI)).to eq(180.0)
+      end
+    end
+  end
+
+  describe "#convert_radiant_to_degree" do
+    context "given an angle in radians" do
+      it "calculate the corresponding angle in degree" do
+        expect(WrfLibrary::SunEquation.convert_radiant_to_degree(0.23212879051524585)).to eq(13.3)
+      end
+    end
+  end
+
+  describe "#convert_radiant_to_degre" do
+    context "given an angle in radians" do
+      it "calculate the corresponding angle in degree" do
+        expect(WrfLibrary::SunEquation.convert_radiant_to_degree(0.39706240482870997)).to eq(22.75)
+      end
+    end
+  end
+
+  describe "#convert_degree_to_radiant" do
+    context "given an angle in degree" do
+      it "calculate the corresponding angle in radians" do
+        expect(WrfLibrary::SunEquation.convert_degree_to_radiant(180)).to eq(Math::PI)
+      end
+    end
+  end
+
+  describe "#convert_degree_to_radiant" do
+    context "given an angle in degree" do
+      it "calculate the corresponding angle in radians" do
+        expect(WrfLibrary::SunEquation.convert_degree_to_radiant(13.3)).to eq(0.23212879051524585)
+      end
+    end
+  end
+
+  describe "#convert_degree_to_radiant" do
+    context "given an angle in degree" do
+      it "calculate the corresponding angle in radians" do
+        expect(WrfLibrary::SunEquation.convert_degree_to_radiant(22.75)).to eq(0.39706240482870997)
       end
     end
   end
