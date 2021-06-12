836afa791e83595b0e81ed7dc67239009bf0fb20
Benjamin Held <me@bheld.eu>
2021-06-12 18:17:54 +0200

    add json converter for hourly wrf data

--- lib/wrf_library/json_converter.rb
+++ lib/wrf_library/json_converter.rb
@@ -2,5 +2,6 @@ module WrfLibrary
   require "json"
   require "wrf_library/json_converter/base_station_converter"
   require "wrf_library/json_converter/wrf_station_converter"
+  require "wrf_library/json_converter/hourly_converter"
   require "wrf_library/json_converter/wrf_converter"
 end
