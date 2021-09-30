80c700b8ae2b7c1085527ed2cc883f0262c8a429
Benjamin Held <me@bheld.eu>
2021-09-30 21:06:34 +0200

    add method to calculate wind direction

--- lib/wrf_library/measurand/wind.rb
+++ lib/wrf_library/measurand/wind.rb
@@ -17,6 +17,19 @@ module WrfLibrary
         wind_speed
       end
 
+      # method to calculate the wind direction from its two dimenstional components
+      # @param [Array] u_component the horizontal wind component
+      # @param [Array] v_component the vertical wind component
+      # @return [Array] the wind directions
+      def self.calculate_winddirection(u_component, v_component)
+        r2d = 180.0 / (Math.atan(1) * 4.0)
+        wind_direction = Array.new()
+        u_component.zip(v_component).each { |u, v| 
+          wind_direction << Math.atan2(u, v) * r2d + 180
+        }
+        wind_direction
+      end
+
     end
 
   end
