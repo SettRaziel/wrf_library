a26873845b59a0a4ecd81a5f195ab3014f1e753b
Benjamin Held <me@bheld.eu>
2023-02-05 11:43:14 +0100

    update doc generation to lib folder only

--- create_yard.sh
+++ create_yard.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# @Author: Benjamin Held
-# @Date:   2015-08-30 09:48:37
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-03-26 17:32:15
 
-yardoc -q './**/*.rb' --private --readme README.md
+yardoc -q './lib/**/*.rb' --private --readme README.md
 printf "Looking for undocumented passages: \n"
-yard stats './**/*.rb' --list-undoc
+yard stats './lib/**/*.rb' --list-undoc
