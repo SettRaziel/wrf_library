39b31c92afd9b285879c5a42ca3e31d5e3496f69
Benjamin Held <me@bheld.eu>
2026-06-04 13:10:38 +0200

    move calculation of humidity to its own module

--- lib/wrf_library/measurand.rb
+++ lib/wrf_library/measurand.rb
@@ -2,6 +2,7 @@ module WrfLibrary
 
   require "wrf_library/measurand/apparent_temperature"
   require "wrf_library/measurand/pressure"
+  require "wrf_library/measurand/relative_humidity"
   require "wrf_library/measurand/wind"
 
 end
--- lib/wrf_library/measurand/apparent_temperature.rb
+++ lib/wrf_library/measurand/apparent_temperature.rb
@@ -19,32 +19,14 @@ module WrfLibrary
         temperature + 0.33 * calculate_water_vapour_pressure(temperature, pressure, humidity) - 0.7 * wind_speed - 4.0
       end
 
-      # calculates the water vapour pressure
-      # @param [Float] temperature the air temperature
-      # @return [Float] the water vapour pressure
-      def self.calculate_saturation_pressure(temperature)
-        6.1094 * Math.exp((17.685 * temperature) / (temperature + 243.04))
-      end
-
-      # calculates the relative humidity
-      # @param [Float] temperature the air temperature in degree celcius
-      # @param [Float] humidity the vapor mixing ratio in kg/kg
-      # @param [Float] pressure the air pressure in hectopascal
-      # @return [Float] the relative humidity in %
-      def self.calculate_relative_humidity(temperature, pressure, humidity)
-        saturation_pressure = calculate_saturation_pressure(temperature)
-        partial_pressure = 0.622 * saturation_pressure / pressure
-        rel_hum_pre = humidity * 100 / partial_pressure
-        [ rel_hum_pre, 100.0 ].min
-      end
-
       # calculates the water vapor pressure for the current condition
       # @param [Float] temperature the air temperature in degree celcius
       # @param [Float] humidity the vapor mixing ratio in kg/kg
       # @param [Float] pressure the air pressure in hectopascal
       # @return [Float] the water vapor pressure
       def self.calculate_water_vapour_pressure(temperature, pressure, humidity)
-        relative_humidity = calculate_relative_humidity(temperature, pressure, humidity)
+        relative_humidity = WrfLibrary::Measurand::RelativeHumidity.
+                              calculate_relative_humidity(temperature, pressure, humidity)
         6.105 * (relative_humidity / 100) * Math.exp((17.27 * temperature) / (temperature + 237.7))
       end
 
